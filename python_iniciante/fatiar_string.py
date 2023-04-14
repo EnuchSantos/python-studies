435949d15346e9646ffb04244a357278e1f6eb52
EnuchSantos <enuchdev@gmail.com>
2023-04-14 08:40:01 -0300

    exercise

---
+++ python_iniciante/fatiar_string.py
@@ -0,0 +1,8 @@
+# Fatiamento de Strings
+
+string = 'Enuch Santos'
+print(string[6:]) # Indo do S ao s
+print(string[-12:-7]) # Indo do E ao h
+print(string[-12:-7:2]) # Indo do E ao h pulando 2
+print(len(string)) # Mostra a quantidade de caracteres
+print(string[::-1]) # Invertendo string
