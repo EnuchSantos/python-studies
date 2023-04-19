0326053bc7b68de62e152736e44ac43f4e13d5cd
EnuchSantos <enuchdev@gmail.com>
2023-04-19 11:02:19 -0300

    while

---
+++ python_iniciante/while.py
@@ -0,0 +1,32 @@
+# count = 0
+# while count < 10:
+#     print('faça sexo amigo')
+#     count += 1
+
+# senha = '1234'
+# passe = False
+
+# while not passe:
+#     senha_test = input("digite sua senha: ")
+#     if senha_test == senha:
+#         passe = True
+
+# while True:
+#     yes_or_not = input("Do you wanna be my girlfriend? 1 - yes 2 - not\n")
+
+#     if yes_or_not == '1':
+#         print('I love you')
+#     elif yes_or_not == '2':
+#         print('Fuck you, your fucking bitch')
+#     else:
+#         break
+
+
+count = 0
+
+while count <= 20:
+    count += 1
+    if count % 2 == 0:
+        continue
+
+    print(f"All numbers odd {count}")
