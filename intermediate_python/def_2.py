92e412abbab3da001726a8d226a936317efefa81
EnuchSantos <enuchdev@gmail.com>
2023-05-17 16:23:54 -0300

    Args

--- intermediate_python/def_2.py
+++ intermediate_python/def_2.py
@@ -1,4 +1,4 @@
-"""_summary_
+"""
 Retorno de valores das funções (return)
 """
 
@@ -12,3 +12,24 @@ def sum_or_plus(a, b):
 sum_1 = sum_or_plus(22, 2)
 sum_2 = sum_or_plus(4, 4)
 print(sum_or_plus(sum_1, sum_2))
+
+"""
+args - Argumentos não nomeadors
+* - *args (empacotamento e desempacotamento)
+"""
+
+x, y, *resto = 1, 2, 3, 4
+print(x, y, resto)
+
+
+def soma(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+
+numeros = 1, 2, 3, 4
+result = soma(*numeros)  # desempacotando
+print(result)
+print(sum((1, 2, 3, 4, 5)))
